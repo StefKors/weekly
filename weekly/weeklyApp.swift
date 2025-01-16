3f5c0bdeab7bfa3212a8f7dbbb14a3033a72b1d9
Stef Kors <stef.kors@gmail.com>
2025-01-16 18:01:25 +0100

    Initial Commit

---
+++ weekly/weeklyApp.swift
@@ -0,0 +1,32 @@
+//
+//  weeklyApp.swift
+//  weekly
+//
+//  Created by Stef Kors on 16/01/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct weeklyApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
